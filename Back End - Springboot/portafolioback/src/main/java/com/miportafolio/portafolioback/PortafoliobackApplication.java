bf6572af2309e08dcdeaea3b91f1f9b092d26e6d
JuanCMulet <73266014+JuanCMulet@users.noreply.github.com>
2022-05-20 19:53:25 -0300

    .

---
+++ Back End - Springboot/portafolioback/src/main/java/com/miportafolio/portafolioback/PortafoliobackApplication.java
@@ -0,0 +1,13 @@
+package com.miportafolio.portafolioback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafoliobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafoliobackApplication.class, args);
+	}
+
+}
